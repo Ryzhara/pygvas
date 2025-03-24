131ebc172abd3336d87f4ed9b89dcbc19e9d9510
Robin Fredericksen <refredericksen@gmail.com>
2025-03-24 16:57:08 -0500

    Fix integer arrays. Need to add bools and floats.

--- gvas/properties/array_property.py
+++ gvas/properties/array_property.py
@@ -108,53 +108,56 @@ class ArrayProperty(PropertyTrait):
 
         self.values = []  # prepare storage
 
-        match self.property_type:
-            case "StructProperty":
-                # Read field type_name
-                self.field_name = read_string(stream)
+        if self.property_type == "StructProperty":
+            # Read field type_name
+            self.field_name = read_string(stream)
 
-                # Read structure sub/generic type
-                array_member_property_type = read_string(stream)
+            # Read structure sub/generic type
+            array_member_property_type = read_string(stream)
 
-                # print(f"Found StructProperty {array_member_property_type=}")
-                assert (
-                    array_member_property_type == self.property_type
-                ), f"Property array member type mismatch: {array_member_property_type} != {self.property_type}"
+            # print(f"Found StructProperty {array_member_property_type=}")
+            assert (
+                array_member_property_type == self.property_type
+            ), f"Property array member type mismatch: {array_member_property_type} != {self.property_type}"
 
-                # Read properties size
-                # TODO: add check for byte count
-                properties_size = struct.unpack("<Q", stream.read(8))[0]
+            # Read properties size
+            # TODO: add check for byte count
+            properties_size = struct.unpack("<Q", stream.read(8))[0]
 
-                self.type_name = read_string(stream)
+            self.type_name = read_string(stream)
 
-                self.guid = read_guid_with_terminator(stream)
+            self.guid = read_guid_with_terminator(stream)
 
-                # now we are supposed to read structs bodies, NO HEADER
-                # Read array elements
-                # print(f"Reading StructProperty members")
-                for idx in range(property_count):
-                    # print(f"Reading StructProperty member {idx=}")
-                    new_array_property = StructProperty(self.property_type)
-                    # print(f"Reading StructProperty member {new_array_property=}")
-                    # have to key the READ from the subtype type_name! aka STRUCT_NAME
-                    new_array_property.read_body(stream)
-                    self.values.append(new_array_property)
+            # now we are supposed to read structs bodies, NO HEADER
+            # Read array elements
+            # print(f"Reading StructProperty members")
+            for idx in range(property_count):
+                # print(f"Reading StructProperty member {idx=}")
+                new_array_property = StructProperty(self.property_type)
+                # print(f"Reading StructProperty member {new_array_property=}")
+                # have to key the READ from the subtype type_name! aka STRUCT_NAME
+                new_array_property.read_body(stream)
+                self.values.append(new_array_property)
 
-            # we probably  need to handle more things in detail? read the code
-            # case "Guid", "DateTime", "Quat", "Vector", "Rotator", etc:
-            #     pass
-            case "StrProperty":
-                for _ in range(property_count):
+        # we probably  need to handle more things in detail? read the code
+        # case "Guid", "DateTime", "Quat", "Vector", "Rotator", etc:
+        #     pass
+        elif self.property_type in ["StrProperty", "NameProperty", "ObjectProperty", "EnumProperty"]:
+            for _ in range(property_count):
                     string_element = read_string(stream)
                     self.values.append(string_element)
-            case _:
 
-                # Read array elements
-                for _ in range(property_count):
-                    new_array_property = Property.new(
-                        stream, self.property_type, include_header=False, options=None
-                    )
-                    self.values.append(new_array_property.value)
+        elif self.property_type == "IntProperty":
+            for _ in range(property_count):
+                    int_element = struct.unpack("<i", stream.read(4))[0]
+                    self.values.append(int_element)
+
+        else:  # catchall
+            for _ in range(property_count):
+                new_array_property = Property.new(
+                    stream, self.property_type, include_header=False, options=None
+                )
+                self.values.append(new_array_property.value)
 
     def write(
         self,
@@ -191,46 +194,49 @@ class ArrayProperty(PropertyTrait):
         array_bytes += array_buffer.write(struct.pack("<I", property_count))
         print(f"\tWriting array: {property_count=}")
         # Handle struct properties
-        match self.property_type:
-            case "StructProperty":
+        if self.property_type == "StructProperty":
 
-                array_bytes += write_string(array_buffer, self.field_name)
+            array_bytes += write_string(array_buffer, self.field_name)
 
-                # Write property type again
-                array_bytes += write_string(array_buffer, self.property_type)
+            # Write property type again
+            array_bytes += write_string(array_buffer, self.property_type)
 
-                properties_byte_count_position = array_buffer.tell()
-                array_bytes += array_buffer.write(struct.pack("<I", 0))
-                array_bytes += array_buffer.write(struct.pack("<I", 0))
+            properties_byte_count_position = array_buffer.tell()
+            array_bytes += array_buffer.write(struct.pack("<I", 0))
+            array_bytes += array_buffer.write(struct.pack("<I", 0))
 
-                array_bytes += write_string(array_buffer, self.type_name)
-                array_bytes += write_guid_with_terminator(array_buffer, self.guid)
+            array_bytes += write_string(array_buffer, self.type_name)
+            array_bytes += write_guid_with_terminator(array_buffer, self.guid)
 
-                # Write properties to array_buffer
-                struct_properties_start = array_buffer.tell()
-                for struct_property in self.values:
-                    array_bytes += struct_property.write(
-                        array_buffer, include_header=False
-                    )
-                struct_properties_end = array_buffer.tell()
-
-                # write total bytes in structs
-                struct_properties_bytes = (
-                    struct_properties_end - struct_properties_start
+            # Write properties to array_buffer
+            struct_properties_start = array_buffer.tell()
+            for struct_property in self.values:
+                array_bytes += struct_property.write(
+                    array_buffer, include_header=False
                 )
-                array_buffer.seek(properties_byte_count_position)
-                array_buffer.write(struct.pack("<I", struct_properties_bytes))
+            struct_properties_end = array_buffer.tell()
 
-            case "StrProperty":
+            # write total bytes in structs
+            struct_properties_bytes = (
+                struct_properties_end - struct_properties_start
+            )
+            array_buffer.seek(properties_byte_count_position)
+            array_buffer.write(struct.pack("<I", struct_properties_bytes))
+
+        elif self.property_type in ["StrProperty", "NameProperty", "ObjectProperty", "EnumProperty"]:
                 for string_value in self.values:
                     array_bytes += write_string(array_buffer, string_value)
 
-            case _:
-                # Write array elements
-                for array_property in self.values:
-                    array_bytes += array_property.write(
-                        array_buffer, include_header=False
-                    )
+        elif self.property_type == "IntProperty":
+            for int_value in self.values:
+                array_bytes += array_buffer.write(struct.pack("<i", int_value))
+
+        else:
+            # Write array elements
+            for array_property in self.values:
+                array_bytes += array_property.write(
+                    array_buffer, include_header=False
+                )
 
         # Write total byte count size
         array_buffer.seek(ap_byte_count_location)
